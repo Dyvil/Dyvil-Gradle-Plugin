31b77484803f6a07724578d956436fc6cac5ce24
Clashsoft <clashsoft@hotmail.com>
2016-01-23 13:33:48 +0100

    Make project buildable

--- src/main/groovy/dyvil/tools/gradle/DyvilPlugin.groovy
+++ src/main/groovy/dyvil/tools/gradle/DyvilPlugin.groovy
@@ -2,14 +2,16 @@ package dyvil.tools.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 
 class DyvilPlugin implements Plugin<Project>
 {
 	@Override
 	void apply(Project project)
 	{
-		project.task('hello') >> {
-			println("Hello World!")
+		Task task = project.task('dyvil')
+		task.doLast {
+			print("Hello Dyvil!")
 		}
 	}
 }
