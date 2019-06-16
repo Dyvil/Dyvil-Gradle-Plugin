fab32cd2743e05452d668e4f4ed6f51c4eefe0f4
Adrian Kunz <clashsoft@hotmail.com>
2019-06-16 16:21:07 +0200

    Add DyvilPlugin

---
+++ src/main/groovy/dyvilx/tools/gradle/DyvilPlugin.groovy
@@ -0,0 +1,10 @@
+package dyvilx.tools.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class DyvilPlugin implements Plugin<Project> {
+	@Override
+	void apply(Project project) {
+	}
+}
